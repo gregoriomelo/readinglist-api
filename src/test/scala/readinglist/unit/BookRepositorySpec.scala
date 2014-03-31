cc82a4b9b55576f50be1b7ef02427dd605893a7f
Gregório Melo <gregorio@gregoriomelo.com>
2014-03-31 12:04:29 +0800

    Refactoring + missing unit tests

--- src/test/scala/readinglist/unit/BookRepositorySpec.scala
+++ src/test/scala/readinglist/unit/BookRepositorySpec.scala
@@ -21,6 +21,17 @@ class BookRepositorySpec extends FunSuite with BeforeAndAfterEach with ShouldMat
     BookRepository.withId(-1) should equal(None)
   }
 
+  test("finds existing book by isbn") {
+    val expectedBook = Book(20, "test", false, "my isbn", true)
+    BookRepository.add(expectedBook)
+
+    BookRepository.withIsbn("my isbn").get should equal(expectedBook)
+  }
+
+  test("does not find missing book by isbn") {
+    BookRepository.withIsbn("there should be nothing with this isbn") should equal(None)
+  }
+
   test("adds a book nicely") {
     BookRepository.add(Book(1, "added book", false, "a isbn", false))
 
@@ -32,5 +43,21 @@ class BookRepositorySpec extends FunSuite with BeforeAndAfterEach with ShouldMat
     BookRepository.add(Book(2, "second book with same ISBN", false, "ISBN", false))
 
     BookRepository.size should equal (1)
+  }
+
+  test("Deletes an existing book") {
+    val book = Book(1, "first book with same isbn", false, "ISBN", false)
+    BookRepository.add(book)
+
+    BookRepository.remove(book)
+
+    BookRepository.size should equal (0)
+  }
+
+  test("Finds all books") {
+    val book = Book(1, "first book with same isbn", false, "ISBN", false)
+    BookRepository.add(book)
+
+    BookRepository.all should equal (List(book))
   }
 }
